0511c5d96e522ffc725f890f7b567a544ee0a8f1
Rodrigo <rperezma.inf@upsa.es>
2024-05-15 11:49:56 +0200

    creacion de views

---
+++ practicafinal/practica_app/views/viewsbiblioteca.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from .models import Biblioteca
+from .forms import BibliotecaForm
+
+# Create your views here.
+
+def listar_bibliotecas(request):
+    bibliotecas = Biblioteca.objects.all()
+    return render(request, 'biblioteca/listar_bibliotecas.html', {'bibliotecas': bibliotecas})
+
+def buscar_biblioteca_nombre(request, nombre):
+    bibliotecas = Biblioteca.objects.filter(nombre__icontains=nombre)
+    return render(request, 'biblioteca/buscar_biblioteca_nombre.html', {'bibliotecas': bibliotecas, 'nombre': nombre})
+
+def buscar_biblioteca_ciudad(request, ciudad):
+    bibliotecas = Biblioteca.objects.filter(ciudad__icontains=ciudad)
+    return render(request, 'biblioteca/buscar_biblioteca_ciudad.html', {'bibliotecas': bibliotecas, 'ciudad': ciudad})
+
+def agregar_biblioteca(request):
+    if request.method == 'POST':
+        form = BibliotecaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_bibliotecas')
+    else:
+        form = BibliotecaForm()
+    return render(request, 'biblioteca/agregar_biblioteca.html', {'form': form})
+
+def editar_biblioteca(request, biblioteca_id):
+    biblioteca = get_object_or_404(Biblioteca, pk=biblioteca_id)
+    if request.method == 'POST':
+        form = BibliotecaForm(request.POST, instance=biblioteca)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_bibliotecas')
+    else:
+        form = BibliotecaForm(instance=biblioteca)
+    return render(request, 'biblioteca/editar_biblioteca.html', {'form': form, 'biblioteca': biblioteca})
+
+def borrar_biblioteca(request, biblioteca_id):
+    biblioteca = get_object_or_404(Biblioteca, pk=biblioteca_id)
+    if request.method == 'POST':
+        biblioteca.delete()
+        return redirect('listar_bibliotecas')
+    return render(request, 'biblioteca/borrar_biblioteca.html', {'biblioteca': biblioteca})
+
+
+
---
+++ practicafinal/practica_app/views/viewslibros.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Libro
+from .forms import LibroForm
+
+# Create your views here.
+
+def listar_libros_biblioteca(request, biblioteca_id):
+    biblioteca = get_object_or_404(Biblioteca, pk=biblioteca_id)
+    libros = biblioteca.libro_set.all()
+    return render(request, 'biblioteca/listar_libros_biblioteca.html', {'biblioteca': biblioteca, 'libros': libros})
+
+def buscar_libro_titulo_biblioteca(request, biblioteca_id, titulo):
+    biblioteca = get_object_or_404(Biblioteca, pk=biblioteca_id)
+    libros = biblioteca.libro_set.filter(titulo__icontains=titulo)
+    return render(request, 'biblioteca/buscar_libro_titulo_biblioteca.html', {'biblioteca': biblioteca, 'libros': libros, 'titulo': titulo})
+
+def buscar_libro_autor_biblioteca(request, biblioteca_id, autor):
+    biblioteca = get_object_or_404(Biblioteca, pk=biblioteca_id)
+    libros = biblioteca.libro_set.filter(autor__icontains=autor)
+    return render(request, 'biblioteca/buscar_libro_autor_biblioteca.html', {'biblioteca': biblioteca, 'libros': libros, 'autor': autor})
+
+def buscar_libro_editorial_biblioteca(request, biblioteca_id, editorial):
+    biblioteca = get_object_or_404(Biblioteca, pk=biblioteca_id)
+    libros = biblioteca.libro_set.filter(editorial__icontains=editorial)
+    return render(request, 'biblioteca/buscar_libro_editorial_biblioteca.html', {'biblioteca': biblioteca, 'libros': libros, 'editorial': editorial})
+
+# Otras vistas para buscar libros
+def buscar_libro_titulo(request, titulo):
+    libros = Libro.objects.filter(titulo__icontains=titulo)
+    return render(request, 'biblioteca/buscar_libro_titulo.html', {'libros': libros, 'titulo': titulo})
+
+def buscar_libro_titulo_disponibilidad(request, titulo):
+    libros = Libro.objects.filter(titulo__icontains=titulo, num_ejemplares__gt=0)
+    return render(request, 'biblioteca/buscar_libro_titulo_disponibilidad.html', {'libros': libros, 'titulo': titulo})
+
+def agregar_libro(request):
+    if request.method == 'POST':
+        form = LibroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_libros')
+    else:
+        form = LibroForm()
+    return render(request, 'biblioteca/agregar_libro.html', {'form': form})
+
+def editar_libro(request, libro_id):
+    libro = get_object_or_404(Libro, pk=libro_id)
+    if request.method == 'POST':
+        form = LibroForm(request.POST, instance=libro)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_libros')
+    else:
+        form = LibroForm(instance=libro)
+    return render(request, 'biblioteca/editar_libro.html', {'form': form, 'libro': libro})
+
+def borrar_libro(request, libro_id):
+    libro = get_object_or_404(Libro, pk=libro_id)
+    if request.method == 'POST':
+        libro.delete()
+        return redirect('listar_libros')
+    return render(request, 'biblioteca/borrar_libro.html', {'libro': libro})
